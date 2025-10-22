6b4e51cdb601fff2b57c913a6e1376c972fdfa85
MichelZero <silva.michel@ifpb.edu.br>
2025-10-22 18:50:06 -0300

    up
    Signed-off-by: MichelZero <silva.michel@ifpb.edu.br>

--- app/src/main/java/com/example/conversordemoedas/MainActivity.kt
+++ app/src/main/java/com/example/conversordemoedas/MainActivity.kt
@@ -41,7 +41,7 @@ interface CurrencyApiService {
 }
 
 class MainActivity : ComponentActivity() {
-    // Substitua "SUA_CHAVE_DE_API" pela chave obetida no site da ExchangeRate-API
+    // Substitua "SUA_CHAVE_DE_API" pela chave obtida no site da ExchangeRate-API
     private val apiKey = "5cc45da295ed4917dd7a8f2c"
 
     override fun onCreate(savedInstanceState: Bundle?) {
